6e0442e88434f6f940de2325854dc1ad72d04610
lolesch <leonid.schreiber@yahoo.de>
2021-11-18 21:19:34 +0100

    First UI scripts for simple buttons

---
+++ Assets/Scripts/UI/Basics/AbstractToggle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI_Showcase
+{
+    [RequireComponent(typeof(Toggle))]
+    public abstract class AbstractToggle : MonoBehaviour
+    {
+        protected Toggle toggle;
+
+        private void Awake()
+        {
+            TryGetComponent(out toggle);
+            toggle?.onValueChanged.AddListener(OnClick);
+        }
+
+        protected abstract void OnClick(bool isOn);
+    }
+}
---
+++ Assets/Scripts/UI/Basics/TestButton.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace UI_Showcase
+{
+    public class TestButton : AbstractButton
+    {
+        protected override void OnClick() => Debug.Log($"{name} was clicked");
+    }
+}
