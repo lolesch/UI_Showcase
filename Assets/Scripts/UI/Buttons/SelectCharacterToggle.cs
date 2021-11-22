a3fed3c98193ed936cec249d89610e3a4b556807
lolesch <leonid.schreiber@yahoo.de>
2021-11-22 11:58:42 +0100

    mouse position highlight on top of category buttons

--- Assets/Scripts/UI/Buttons/SelectCharacterToggle.cs
+++ Assets/Scripts/UI/Buttons/SelectCharacterToggle.cs
@@ -6,22 +6,18 @@ namespace UI_Showcase
 {
     public class SelectCharacterToggle : AbstractToggle
     {
-        [SerializeField] private List<MaskableGraphic> selectedLightColorChangeObjects;
-        [SerializeField] private List<MaskableGraphic> selectedDarkColorChangeObjects;
+        [SerializeField] private List<MaskableGraphic> selectedColorChangeObjects;
 
-        [SerializeField] private SettableColor selectedLightColor;
-        [SerializeField] private SettableColor selectedDarkColor;
+        [SerializeField] private SettableColor selectedColor;
         [SerializeField] private SettableColor deselectedColor;
 
         protected override void OnValueChanged(bool isOn)
         {
-            foreach (var item in selectedLightColorChangeObjects)
-                item.color = isOn ? selectedLightColor.color : deselectedColor.color;
-
-            foreach (var item in selectedDarkColorChangeObjects)
-                item.color = isOn ? selectedDarkColor.color : deselectedColor.color;
+            foreach (var item in selectedColorChangeObjects)
+                item.color = isOn ? selectedColor.color : deselectedColor.color;
         }
 
+        [ContextMenu("Validate")]
         private void OnValidate()
         {
             if (TryGetComponent(out toggle))
